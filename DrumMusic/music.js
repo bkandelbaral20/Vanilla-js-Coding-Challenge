c30202ee9981e7cdbf1bc63193e44cb5a86da25c
Binjita <binjita.baral@gmail.com>
2021-01-04 20:44:51 -0600

    played sounds according to the keycodes

---
+++ DrumMusic/music.js
@@ -0,0 +1,24 @@
+
+//adding event listener
+ window.addEventListener('keydown', function (e) {
+
+     //now it is selecting audio element for each keyCode
+     const sound = document.querySelector(`audio[data-key="44"]`);
+     sound.play();
+     const sound1 = document.querySelector(`audio[data-key="45"]`);
+     sound1.play();
+     const sound2 = document.querySelector(`audio[data-key="46"]`);
+     sound2.play();
+     const sound3 = document.querySelector(`audio[data-key="47"]`);
+     sound3.play();
+     const sound4 = document.querySelector(`audio[data-key="48"]`);
+     sound4.play();
+     const sound5 = document.querySelector(`audio[data-key="49"]`);
+     sound5.play();
+     const sound6 = document.querySelector(`audio[data-key="50"]`);
+     sound6.play();
+     const sound7 = document.querySelector(`audio[data-key="37"]`);
+     sound7.play();
+
+
+     });
