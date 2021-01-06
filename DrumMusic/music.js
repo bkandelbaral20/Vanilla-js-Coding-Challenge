df2562711b3ec43e46a6628ef28a8a4c8880ee25
Binjita <binjita.baral@gmail.com>
2021-01-05 19:49:02 -0600

    styling key codes

--- DrumMusic/music.js
+++ DrumMusic/music.js
@@ -1,23 +1,27 @@
-
 //adding event listener
- window.addEventListener('keydown', function (e) {
+window.addEventListener('keydown', function (e) {
 
-     //now it is selecting audio element for each keyCode
-     const sound = document.querySelector(`audio[data-key=${e.code}]`);
-     console.log(e.code);
+    //now it is selecting audio element for each keyCode
+    const sound = document.querySelector(`audio[data-key=${e.code}]`);
+    const key = document.querySelector(`.key[data-key=${e.code}]`);
 
-     /*
-     if it is not the audio keycode then function will break from here and does not run the other commands
-      */
+    console.log(e.code);
 
-     if(!sound) return;
+    /*
+    if it is not the audio keycode then function will break from here and does not run the other commands
+     */
 
-     /*
-     All these sounds are 3sec long so when ever user press keys often, it takes time to play from beginning
-     so to avoid this, we are setting time to 0 to rewind it.
-      */
+    if (!sound) return;
 
-     sound.currentTime = 0;
-     sound.play();
+    /*
+    All these sounds are 3sec long so when ever user press keys often, it takes time to play from beginning
+    so to avoid this, we are setting time to 0 to rewind it.
+     */
 
-     });
+    sound.currentTime = 0;
+    sound.play();
+
+//    adding new class into key so that it will show the transform style
+    key.classList.add('press');
+
+});
