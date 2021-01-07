f9fd57945311ced3987be3963e502aa4a20fff5c
Binjita <binjita.baral@gmail.com>
2021-01-06 19:52:49 -0600

    clock works perfectly

--- Clock/clock.js
+++ Clock/clock.js
@@ -2,12 +2,14 @@
 //lets make clock function-able
 
 const secondHand = document.querySelector('.hand-second');
+const minHand = document.querySelector('.hand-minute');
+const hourHand = document.querySelector('.hand-hour');
 
 function setDate() {
     //creating instance of now to get the actual time ( hours, mins and second)
     const now = new Date();
 
-    //*************for seconds
+    //************* for seconds ***************
     const second = now.getSeconds();
     // console.log(second);
 // now we are getting seconds into number but we have to changed it into degree in a circle
@@ -16,7 +18,17 @@ function setDate() {
     secondHand.style.transform =`rotate(${secondDegree}deg)`;
 
 
+    //************* for minute *****************
+    const min = now.getMinutes();
+    const minDegree = ((min/60) * 360)+90;
+    minHand.style.transform=`rotate(${minDegree}deg)`;
 
+
+    //************* for hour *****************
+    const hour = now.getHours();
+    //we are dividing 12 because we have 12 hours in clock
+    const hourDegree = ((hour/12) * 360)+90;
+    hourHand.style.transform=`rotate(${hourDegree}deg)`;
 }
 
 setInterval(setDate, 1000);
