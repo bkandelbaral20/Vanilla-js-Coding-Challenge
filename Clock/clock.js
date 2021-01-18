f22ff0967248b18e13c0fe5d1c1e549c80018ac9
Binjita <binjita.baral@gmail.com>
2021-01-17 21:11:21 -0600

    created backbone

--- Clock/clock.js
+++ Clock/clock.js
@@ -12,23 +12,24 @@ function setDate() {
     //************* for seconds ***************
     const second = now.getSeconds();
     // console.log(second);
+
 // now we are getting seconds into number but we have to changed it into degree in a circle
 //we are adding 90 at last because by default it rotate left to right but we want to rotate it top-bottom(12-6).
-    const secondDegree = ((second/60) * 360) + 90;
-    secondHand.style.transform =`rotate(${secondDegree}deg)`;
+    const secondDegree = ((second / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
 
     //************* for minute *****************
     const min = now.getMinutes();
-    const minDegree = ((min/60) * 360)+90;
-    minHand.style.transform=`rotate(${minDegree}deg)`;
+    const minDegree = ((min / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minDegree}deg)`;
 
 
     //************* for hour *****************
     const hour = now.getHours();
     //we are dividing 12 because we have 12 hours in clock
-    const hourDegree = ((hour/12) * 360)+90;
-    hourHand.style.transform=`rotate(${hourDegree}deg)`;
+    const hourDegree = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
 
 setInterval(setDate, 1000);
